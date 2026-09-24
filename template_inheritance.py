88fdca12ed96668a305cdf0250ee8fef0d2f2861
Ivan Vasilev <i_vasilev@ymail.com>
2026-09-24 13:44:45 +0200

    add active page adn class in navi

--- template_inheritance.py
+++ template_inheritance.py
@@ -9,11 +9,10 @@ navigation = [
 
 @app.route('/')
 def index():
-    return render_template('template_inheritance_index.html', navigation=navigation)
+    return render_template('template_inheritance_index.html', navigation=navigation, active_page = "Home")
 
 @app.route('/about')
 def about():
-    return render_template('template_inheritance_about.html', navigation=navigation)
-
+    return render_template('template_inheritance_about.html', navigation=navigation, active_page = "About")
 if __name__ == '__main__':
     app.run(debug=True)
